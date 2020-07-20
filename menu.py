cc2833356ca534a1459bbeffd15dfb9422ebc648
Jacob Preston <prestja@gmail.com>
2020-07-19 22:15:53 -0500

    Main menu option selection

--- menu.py
+++ menu.py
@@ -2,13 +2,20 @@ class BaseMenu:
     def display(self):
         pass
 class MainMenu(BaseMenu):
-    amt = 0
     def display(self):
+        i = -1
         print("LEGO Management System")
         print("You are not logged in!")
         print("---------------------\n")
-        print("[0] Log in as customer (online mode)")
-        print("[1] Log in as employee (store mode)")
-        print("[2] Exit system")
-        i = input("Please make a selection: ")
-		
+        print("[1] Log in as customer (online mode)")
+        print("[2] Log in as employee (store mode)")
+        print("[3] Exit system")
+        i = int(input("Please make a selection: "))
+        while i < 1 or i > 3:
+            i = int(input("Please make a selection: "))
+        if i == 1:
+            print("Selection 1")
+        if i == 2:
+            print("Selection 2")
+        if i == 3:
+            print("Selection 3")
