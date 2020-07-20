6b532b295696b8a43a82f61c66e2cc6fb8c4049a
Jacob Preston <prestja@gmail.com>
2020-07-19 22:01:16 -0500

    Example hook into SQL db

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from menu import *
+import mysql.connector
 
 def main():
     a = MainMenu()
     a.display()
+	
+
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="CSce4650!"
+)
+
+print(db)
 main()
