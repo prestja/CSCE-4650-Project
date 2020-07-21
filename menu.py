969f758a56c8ad5b0127ee0fbb0e4e59f5f246dc
Jacob Preston <prestja@gmail.com>
2020-07-21 11:52:27 -0500

    Username, password prompts for employee and customer

--- menu.py
+++ menu.py
@@ -4,24 +4,24 @@ class BaseMenu:
 class MainMenu(BaseMenu):
     subMenu = None
     def display(self):
-        i = -1
-        print("LEGO Management System")
-        print("You are not logged in!")
-        print("---------------------\n")
-        print("[1] Log in as customer (online mode)")
-        print("[2] Log in as employee (store mode)")
-        print("[3] Exit system")
-        i = int(input("Please make a selection: "))
-        while i < 1 or i > 3:
+        d = 0
+        while d < 1:
+            i = -1
+            print("LEGO Management System")
+            print("You are not logged in!")
+            print("---------------------\n")
+            print("[1] Log in as customer (online mode)")
+            print("[2] Log in as employee (store mode)")
+            print("[3] Exit system")
             i = int(input("Please make a selection: "))
-        if i == 1:
-            subMenu = LoginMenu(0)
-            subMenu.display()
-        if i == 2:
-            subMenu = LoginMenu(1)
-            subMenu.display()
-        if i == 3:
-            quit()
+            if i == 1:
+                subMenu = LoginMenu(0)
+                subMenu.display()
+            if i == 2:
+                subMenu = LoginMenu(1)
+                subMenu.display()
+            if i == 3:
+                quit()
 
 class LoginMenu(BaseMenu):
     asEmployee = 0
@@ -32,5 +32,10 @@ class LoginMenu(BaseMenu):
     def display(self):
         if self.asEmployee == 1:
             print("Login as employee")
+            id = input("Enter your employee ID: ")
+            pswd = input("Enter your password: ")
         else:
             print ("Login as customer")
+            usr = input("Enter your username: ")
+            pswd = input("Enter your password: ")
+            
